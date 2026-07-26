e46ee32d9c90af364226511e83d588641af8729b
Sean Villamonte <seanpajarillo@gmail.com>
2026-07-26 03:34:39 -0400

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const usersController = require('../controllers/userController');
 
 router.post("/", usersController.add);
+router.get("/", usersController.list);
+router.get("/:id", usersController.read);
+router.put("/:id", usersController.update);
+router.delete("/:id", usersController.remove);
 
 module.exports = router;
