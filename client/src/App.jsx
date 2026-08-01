044bd64ada8a2d7102d05e8cb6ca2fdd5f3e4274
Sean Villamonte <seanpajarillo@gmail.com>
2026-07-31 23:41:46 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import MainRouter from "../MainRouter";
 
 const App = () => {
-    return (
-        // The BrowserRouter component is used to enable routing in the application
-        // The MainRouter component contains the different routes for the application
-        <BrowserRouter>
-        <MainRouter /> 
-        </BrowserRouter>
-    );
+    return <MainRouter />;
 }
 export default App;
